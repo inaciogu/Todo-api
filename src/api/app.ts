655bfe1a96e8dba6c9d6fd90a28388e1c1a43ba3
Gustavo <gus.inacio@hotmail.com>
2022-05-18 11:46:29 -0300

    feat: Projects initial configuration.

---
+++ src/api/app.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+export default class App {
+  public app: express.Application;
+
+  constructor() {
+    this.app = express();
+    this.app.use(express.json());
+  }
+
+  public startServer(PORT: string | number = 3001): void {
+    this.app.listen(PORT, () => console.log(`Server running here 👉 http://localhost:${PORT}`));
+  }
+}
