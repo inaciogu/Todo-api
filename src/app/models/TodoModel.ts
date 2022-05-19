71b8bef8426a10bf320ed5a551bd5aa11691cef3
Gustavo <gus.inacio@hotmail.com>
2022-05-19 09:40:42 -0300

    feat: Todo model

---
+++ src/app/models/TodoModel.ts
@@ -0,0 +1,16 @@
+import { Schema, model as createModel, Document } from 'mongoose';
+import { Todo } from '../interfaces/todo';
+import MongoModel from './MongoModel';
+
+interface TodoDocument extends Todo, Document {}
+
+const TodoSchema = new Schema<TodoDocument>({
+  name: String,
+  status: String,
+});
+
+export default class TodoModel extends MongoModel<Todo> {
+  constructor(model = createModel('Todo', TodoSchema)) {
+    super(model);
+  }
+}
