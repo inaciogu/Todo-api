891cd6a8a7a3a697f6961a0d88f9bca6b4da5e70
Gustavo <gus.inacio@hotmail.com>
2022-05-19 10:00:52 -0300

    feat: Todo services create method.

---
+++ src/app/services/TodoService.ts
@@ -0,0 +1,19 @@
+import { ZodError } from 'zod';
+import { Todo, todoSchema } from '../interfaces/todo';
+import TodoModel from '../models/TodoModel';
+
+export interface ServiceError {
+  error: ZodError;
+}
+
+export default class TodoService {
+  constructor(public model = new TodoModel()) {}
+
+  async create(obj: Todo): Promise<Todo | ServiceError> {
+    const parsed = todoSchema.safeParse(obj);
+    if (!parsed.success) {
+      return { error: parsed.error };
+    }
+    return this.model.create(obj);
+  }
+}
