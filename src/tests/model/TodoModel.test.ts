fb0c280914d99ce885463f23ce51edda33e8cd53
Gustavo <gus.inacio@hotmail.com>
2022-05-20 11:04:09 -0300

    test: Create and list methods successful cases

--- src/tests/model/TodoModel.test.ts
+++ src/tests/model/TodoModel.test.ts
@@ -2,3 +2,56 @@ import sinon from 'sinon';
 import mongoose from 'mongoose';
 import { expect } from 'chai';
 import TodoModel from '../../app/models/TodoModel';
+
+const mockedTodo = {
+  name: 'Estudar',
+  status: 'pendente',
+};
+
+const mockedTodos = [
+  {
+    name: 'Estudar',
+    status: 'pendente',
+  },
+  {
+    name: 'malhar',
+    status: 'pendente',
+  },
+];
+
+const todoModel = new TodoModel();
+
+describe('Todo Model tests', () => {
+  describe('1 - "Create" method', () => {
+    before(async () => {
+      sinon.stub(mongoose.Model, 'create').resolves(mockedTodo);
+    });
+
+    after(() => {
+      sinon.restore();
+    });
+
+    it('Creates a todo item and return it', async () => {
+      const response = await todoModel.create({
+        name: 'Estudar',
+        status: 'pendente',
+      });
+      expect(response).to.be.deep.equal(mockedTodo);
+    });
+  });
+
+  describe('2 - "List" method', () => {
+    before(async () => {
+      sinon.stub(mongoose.Model, 'find').resolves(mockedTodos);
+    });
+
+    after(() => {
+      sinon.restore();
+    });
+
+    it('Returns a array with all todos', async () => {
+      const response = await todoModel.list();
+      expect(response).to.be.deep.equal(mockedTodos);
+    });
+  });
+});
