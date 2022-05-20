a979eece61815af6d264c05bb21b52beec87e773
Gustavo <gus.inacio@hotmail.com>
2022-05-20 08:58:12 -0300

    feat: Create a todo router

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,5 +1,14 @@
 import App from './app';
+import TodoController from '../app/controllers/TodoController';
+import CustomRouter from '../routes/Router';
 
 const server = new App();
+
+const todoRouter = new CustomRouter();
+const todoController = new TodoController();
+
+todoRouter.addRoute(todoController);
+
+server.addRouter(todoRouter.router);
 
 server.startServer();
