a1a8384d89d426c583ddb5413a1de2c5fda4bf49
Gustavo <gus.inacio@hotmail.com>
2022-05-20 09:15:32 -0300

    feat: uses cors to allow other apps to make requests

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,4 +1,5 @@
 import express, { Router } from 'express';
+import cors from 'cors';
 import connectDatabase from './connection';
 
 export default class App {
@@ -7,6 +8,7 @@ export default class App {
   constructor() {
     this.app = express();
     this.app.use(express.json());
+    this.app.use(cors());
   }
 
   public startServer(PORT: string | number = 3001): void {
