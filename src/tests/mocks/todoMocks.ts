cf12a501f45cf16abea9499358394c261bf7cca6
Gustavo <gus.inacio@hotmail.com>
2022-05-20 11:16:56 -0300

    test: Mocked responses to use in the tests.

---
+++ src/tests/mocks/todoMocks.ts
@@ -0,0 +1,18 @@
+export const todoReturn = {
+  _id: 'asasd1sd3441ds',
+  name: 'Estudar',
+  status: 'pendente',
+};
+
+export const todoArray = [
+  {
+    _id: '123das231ds',
+    name: 'Estudar',
+    status: 'pendente',
+  },
+  {
+    _id: 'asdasdbk1231',
+    name: 'malhar',
+    status: 'pendente',
+  },
+];
