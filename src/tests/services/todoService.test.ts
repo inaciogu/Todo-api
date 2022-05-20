14d792ada76b454baa07bc80f241651e86633345
Gustavo <gus.inacio@hotmail.com>
2022-05-20 11:26:25 -0300

    test: List and create method from service layer.

--- src/tests/services/todoService.test.ts
+++ src/tests/services/todoService.test.ts
@@ -1,4 +1,38 @@
 import sinon from 'sinon';
-import mongoose from 'mongoose';
 import { expect } from 'chai';
-import TodoModel from '../../app/models/TodoModel';
+import TodoService from '../../app/services/TodoService';
+import { todoArray, todoReturn } from '../mocks/todoMocks';
+
+describe('Todo service layer tests', () => {
+  const todoService = new TodoService();
+
+  describe('"Create" method', () => {
+    before(async () => {
+      sinon.stub(todoService.model, 'create').resolves(todoReturn);
+    });
+
+    after(() => {
+      sinon.restore();
+    });
+
+    it('Returns the created todo item', async () => {
+      const response = await todoService.create(todoReturn);
+      expect(response).to.be.deep.equal(todoReturn);
+    });
+  });
+
+  describe('"List" method', () => {
+    before(async () => {
+      sinon.stub(todoService.model, 'list').resolves(todoArray);
+    });
+
+    after(() => {
+      sinon.restore();
+    });
+
+    it('Returns all todo items', async () => {
+      const response = await todoService.list();
+      expect(response).to.be.deep.equal(todoArray);
+    });
+  });
+});
